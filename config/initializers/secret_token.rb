741b6d7f13dcd6ec8ef0211d7f206f73752c17be
Howard Fackrell <howard.fackrell@octanner.com>
2013-06-25 09:13:51 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KataIchi::Application.config.secret_token = 'e3d34853c4f227d23c41cc409bd1bc5c19e88ab9cbe5e08330363738a1aba8ce0e6743f62998f75418b65874f9c0c0b83532b75cb74f86a67801a7152abaf8ee'
